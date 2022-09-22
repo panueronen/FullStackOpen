7d3c3adaef7e6ab24c8d6f54df3db00bcea84778
panueronen <panu.eronen@gmail.com>
2022-09-22 23:55:13 +0300

    task 2.6

---
+++ Osa2/part2/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const App = (props) => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    event.target.reset()
+    /*
+    const addNote = (event) => {
+      event.preventDefault()
+      const noteObject = {
+        content: newNote,
+        date: new Date().toISOString(),
+        important: Math.random() > 0.5,
+        id: notes.length + 1,
+      }
+    
+      setNotes(notes.concat(noteObject))
+      setNewNote('')
+      
+    }
+    */
+  }
+  
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+        <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+
+}
+
+
+
+export default App
